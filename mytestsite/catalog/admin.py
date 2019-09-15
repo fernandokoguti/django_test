5cd3f81acc6d8b4e4c57b3cfd7404d71c3b78289
fernandokoguti <fernando.koguti@gmail.com>
2019-09-15 18:08:15 -0300

    atualização de bD (SQL3 pra PostGresql) e avançando pra parte 4 do TUtorial developer.mozilla.org

--- mytestsite/catalog/admin.py
+++ mytestsite/catalog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from catalog.models import Author, Genre, Book, BookInstance
+
+admin.site.register(Book)
+#admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(BookInstance)
+
+# Define the admin class
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+# Register the admin class with the associated model
+admin.site.register(Author, AuthorAdmin)
+
